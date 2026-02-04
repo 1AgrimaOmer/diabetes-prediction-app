683bd9332c34c03eb6f2531ba2e904b10e838a21
Agrima <agrimaomer2004@gmail.com>
2026-02-04 23:19:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+# Load model and scaler
+model = joblib.load("diabetes_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.title("Diabetes Prediction App 🩺")
+st.write("Enter patient details to predict diabetes:")
+
+preg = st.number_input("Pregnancies", 0, 20)
+glucose = st.number_input("Glucose", 0, 200)
+bp = st.number_input("Blood Pressure", 0, 150)
+skin = st.number_input("Skin Thickness", 0, 100)
+insulin = st.number_input("Insulin", 0, 900)
+bmi = st.number_input("BMI", 0.0, 70.0)
+dpf = st.number_input("Diabetes Pedigree Function", 0.0, 3.0)
+age = st.number_input("Age", 1, 120)
+
+if st.button("Predict"):
+    input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+
+    if prediction[0] == 1:
+        st.error("⚠️ The person is likely Diabetic")
+    else:
+        st.success("✅ The person is likely Not Diabetic")
